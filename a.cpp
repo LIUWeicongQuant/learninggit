faa542dbaa12c211aa6123d3db8c407ae8df41ee
LIUWeicongQuant <liuweicong1990@163.com>
2017-06-04 10:02:17 +0800

    A new branch

--- a.cpp
+++ a.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
 	cout << "Hello World!" << endl;
 	cout << "Modified version of hello world!" << endl;
+	cout << "A version on the dev branch" << endl;
 	return 0;
 }
